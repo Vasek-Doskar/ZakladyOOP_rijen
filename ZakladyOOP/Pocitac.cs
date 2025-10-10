eea1345ad53f90877600296dcee6465edfc3e3d8
Vasek-Doskar <doskar@spsjc.cz>
2025-10-10 09:10:22 +0200

    Přidejte soubory projektu.

---
+++ ZakladyOOP/Pocitac.cs
@@ -0,0 +1,58 @@
+﻿namespace ZakladyOOP
+{// Třída je POUZE předpis (postup, návod) jak se objekty budou tvořit
+    public class Pocitac
+    {
+        private int _ram;
+        private string _grafika;
+        private string _procesor;
+        private string _name;
+        private bool _zapnuty = false;
+        private string _prijataZprava = "";
+
+
+        public Pocitac(int ram, string grafika, string procesor, string name)
+        {
+            _ram = ram;
+            _grafika = grafika;
+            _procesor = procesor;
+            _name = name;
+        }
+
+
+        public string VratJmeno() => _name;
+
+
+        public void Zapnout()
+        {
+            if (!_zapnuty) // (zapnuty == false)
+            {
+                _zapnuty = true;
+            }
+        }
+
+        public void Vypnout()
+        {
+            if (_zapnuty) // (zapnuty == true)
+                _zapnuty = false;
+        }
+
+        public bool JeZapnuty() => _zapnuty;
+
+        public void PustitFilm() => Console.WriteLine(
+            _zapnuty ?
+            "Sleduju fantastickou 4" :
+            "Počítač je vypnutý!");
+
+
+        public void OdesliZpravu(string zprava, Pocitac pocitac)
+        {
+            pocitac.PrijmiZpravu(zprava,_name);
+        }
+
+        private void PrijmiZpravu(string zprava,string jmeno)
+        {
+            _prijataZprava = zprava;
+            Console.WriteLine($"{_name}: Byla přijata zpráva od {jmeno}: " + _prijataZprava);
+        }
+    }
+}
---
+++ ZakladyOOP/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ZakladyOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //vytvoření instance třídy Pocitac
+            Pocitac Dell = new Pocitac(64,"i5","GeForce RTX 950","dell");
+            Dell.Zapnout();
+            Console.WriteLine(Dell.JeZapnuty() ? "Počítač je zapnutý":"Počítač je vypnutý");
+            //Dell.Vypnout();
+            //Console.WriteLine(Dell.JeZapnuty() ? "Počítač je zapnutý" : "Počítač je vypnutý");
+            Dell.PustitFilm();
+
+            Pocitac HP = new Pocitac(16, "i5", "GeForce RTX 950","hp");
+            Dell.OdesliZpravu("Hello World", HP);
+        }
+    }
+}
