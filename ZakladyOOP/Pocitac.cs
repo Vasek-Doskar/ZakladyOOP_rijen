6f990f02c63dcf31df292fe19465e05cdf67aa2b
Vasek-Doskar <doskar@spsjc.cz>
2025-10-24 09:23:22 +0200

    acer

--- ZakladyOOP/Pocitac.cs
+++ ZakladyOOP/Pocitac.cs
@@ -5,26 +5,79 @@
         /// <summary>
         /// Toto je dokumentační komentář :)
         /// </summary>
+        /// 
+        // Atributy (členské proměnné)
         private int _ram;
         private string _grafika;
         private string _procesor;
-        private string _name;
+        private string _jmeno;
+
         private bool _zapnuty = false;
         private string _prijataZprava = "";
 
 
-        public Pocitac(int ram, string grafika, string procesor, string name)
+        // konstruktor - první metoda která vytváří instanci objektu přes klíčové slovo new
+        public Pocitac(int ram, string grafika, string procesor, string jmeno)
         {
-            _ram = ram;
-            _grafika = grafika;
+            Ram = ram;
+            Grafika = grafika;
             _procesor = procesor;
-            _name = name;
+            Jmeno = jmeno;
         }
 
 
-        public string VratJmeno() => _name;
+        //public bool JeZapnuty { get => _zapnuty; }
+        public bool JeZapnuty => _zapnuty; // jen pro čtení
+        public string Procesor
+        {
+            get => _procesor;
+            set
+            {
+                if (value.Length > 0)
+                {
+                    _procesor = value;
+                }
+            }
+        }
+
+        public string Jmeno
+        {
+            get { return _jmeno; }
+            set
+            {
+                if (value.Length > 0)
+                {
+                    _jmeno = value;
+                }
+            }
+        }
+
+        public string Grafika
+        {
+            get => _grafika;
+            set
+            {
+                if (value.Length > 0)
+                {
+                    _grafika = value;
+                }
+            }
+        }
+
+        public int Ram
+        {
+            get => _ram;
+            set
+            {
+                if (value > 0)
+                {
+                    _ram = value;
+                }
+            }
+        }
 
 
+        // metoda třídy Pocitac
         public void Zapnout()
         {
             if (!_zapnuty) // (zapnuty == false)
@@ -39,8 +92,6 @@
                 _zapnuty = false;
         }
 
-        public bool JeZapnuty() => _zapnuty;
-
         public void PustitFilm() => Console.WriteLine(
             _zapnuty ?
             "Sleduju fantastickou 4" :
@@ -49,13 +100,13 @@
 
         public void OdesliZpravu(string zprava, Pocitac pocitac)
         {
-            pocitac.PrijmiZpravu(zprava,_name);
+            pocitac.PrijmiZpravu(zprava, _jmeno);
         }
 
-        private void PrijmiZpravu(string zprava,string jmeno)
+        private void PrijmiZpravu(string zprava, string jmeno)
         {
             _prijataZprava = zprava;
-            Console.WriteLine($"{_name}: Byla přijata zpráva od {jmeno}: " + _prijataZprava);
+            Console.WriteLine($"{_jmeno}: Byla přijata zpráva od {jmeno}: " + _prijataZprava);
         }
     }
 }
--- ZakladyOOP/Program.cs
+++ ZakladyOOP/Program.cs
@@ -6,14 +6,16 @@
         {
             //vytvoření instance třídy Pocitac
             Pocitac Dell = new Pocitac(64,"i5","GeForce RTX 950","dell");
-            Dell.Zapnout();
-            Console.WriteLine(Dell.JeZapnuty() ? "Počítač je zapnutý":"Počítač je vypnutý");
-            //Dell.Vypnout();
-            //Console.WriteLine(Dell.JeZapnuty() ? "Počítač je zapnutý" : "Počítač je vypnutý");
-            Dell.PustitFilm();
 
-            Pocitac HP = new Pocitac(16, "i5", "GeForce RTX 950","hp");
-            Dell.OdesliZpravu("Hello World", HP);
+            Dell.Jmeno = "";
+            Console.WriteLine(Dell.Jmeno); // dell
+            Dell.Jmeno = "Dell";
+            Console.WriteLine(Dell.Jmeno); // Dell
+            Dell.Zapnout();
+            Console.WriteLine(Dell.JeZapnuty);
+
+            Pocitac Acer = new(32, "i7", "GeForce RTX 950", "acer");
+
         }
     }
 }
